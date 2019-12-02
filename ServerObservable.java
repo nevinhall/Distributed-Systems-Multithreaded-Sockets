1504a883c77806686941074619b997654d941666
nevinhall <46722804+nevinhall@users.noreply.github.com>
2019-12-02 15:08:40 +0000

    Revert "Revert "update method now finds a villain with a given name adds to an array list and then deletes the villain from the directory""
    This reverts commit cdf82d42b12eb52b7b9dfabbfa8e55507fa4abd3.

--- ServerObservable.java
+++ ServerObservable.java
@@ -1,25 +1,25 @@
+import java.io.File;
 import java.util.Observable;
 
 public class ServerObservable extends Observable {
-<<<<<<< Updated upstream
-=======
+
 	public void tempFile(File fileDir) {
 
 		//loop doesnt work goes too far
 		Boolean exists = true;
-		
+
 		while(exists == true){
 			System.out.println("The given Directory exists, notifying observers");
 			setChanged();
 			System.out.println("reached");
 			notifyObservers(fileDir);
 			exists = fileDir.exists() && fileDir.list().length != 1;
-			
-			
+
+
 			if(fileDir.list().length == 1){
 				exists = false;
 				//have to also tell client to run
-				
+
 
 			}
 
@@ -28,6 +28,6 @@ public class ServerObservable extends Observable {
 
 
 	}
->>>>>>> Stashed changes
+
 
 }
