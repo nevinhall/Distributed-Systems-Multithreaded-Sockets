bb57288e307ff53e4c45bdab3516f96278839e51
nevinhall <46722804+nevinhall@users.noreply.github.com>
2019-12-02 15:11:57 +0000

    re-configured obserable/observer now finds a file add to array list and deletes file and directory

--- HeroClient.java
+++ HeroClient.java
@@ -6,30 +6,7 @@ import java.net.Socket;
 
 public class HeroClient {
 
-<<<<<<< Updated upstream
-	public static void main(String[] args) {
-		  String host = "localhost";
-		 try {
-		        // Establish connection with the server
-		        Socket socket = new Socket(host, 8007);
 
-		        // Create an output stream to the server
-		        DataOutputStream toServer = new DataOutputStream(socket.getOutputStream());
-		        DataInputStream inputFromClient = new DataInputStream(socket.getInputStream());
-		        
-		        Object object = inputFromClient.readBoolean();
-		        System.out.println("sent from server " + object);
-
-		        toServer.writeBoolean(true);
-		        
-		        
-		        
-		      }
-		      catch (IOException ex) {
-		        ex.printStackTrace();
-		      }
-		// TODO Auto-generated method stub
-=======
 	public static void main(String[] args) throws ClassNotFoundException  {
 		String host = "localhost";
 		try {
@@ -38,29 +15,29 @@ public class HeroClient {
 
 
 			//Allows test value 
-//			DataInputStream fromServerBoolean = new DataInputStream(socket.getInputStream());
-//			Object test =  fromServerBoolean.readBoolean();
-//			System.out.println("value is" + test.toString());
-//			fromServerBoolean.close();
-			
+			//			DataInputStream fromServerBoolean = new DataInputStream(socket.getInputStream());
+			//			Object test =  fromServerBoolean.readBoolean();
+			//			System.out.println("value is" + test.toString());
+			//			fromServerBoolean.close();
 
-			
+
+
 			while (true) {
 				// Establish connection with the server
 				new ServerHandler(socket).start();;
 			}
-		
 
 
-			}
-			catch (IOException ex) {
-				ex.printStackTrace();
-			}
-		
-					 
+
 		}
->>>>>>> Stashed changes
+		catch (IOException ex) {
+			ex.printStackTrace();
+		}
+
 
 	}
 
+
 }
+
+
--- Server.java
+++ Server.java
@@ -4,48 +4,15 @@ import java.net.*;
 public class Server {
 
 	private DataInputStream inputFromClient;
-
-	public static void main(String[] args) throws ClassNotFoundException {
+	
+	public static void main(String[] args) throws ClassNotFoundException, IOException {
 		new Server();
+		
 	}
 
-<<<<<<< Updated upstream
-	public Server() {
-		try {
-			// Create a server socket
-			ServerSocket serverSocket = new ServerSocket(8007);
-			System.out.println("Server started ");
-			while (true) {
-				// Listen for a new connection request
-				Socket socket = serverSocket.accept();
-
-				// Create an input stream from the socket
-				inputFromClient = new DataInputStream(socket.getInputStream());
-				DataOutputStream 	toClientBoolean = new DataOutputStream(socket.getOutputStream());
-				
-				toClientBoolean.writeBoolean(false);
-				
-
-				// Read from input
-				Object object = inputFromClient.readBoolean();
-//				System.out.println("sent from client " + object);
-//				System.out.println("im the server");
-				
-				
-
-		
-			
-			//	serverSocket.close();
-			}
-
-		}
-		catch(IOException ex) {
-			ex.printStackTrace();
-		}
-=======
-	public Server() throws ClassNotFoundException {
+	public Server() throws ClassNotFoundException, IOException {
 		ServerSocket serverSocket = null;
-		Socket socket = null;
+
 
 		// Create a server socket
 
@@ -57,37 +24,42 @@ public class Server {
 		}
 		System.out.println("Server started ");
 
-		try {
-			socket = serverSocket.accept();
-			
-
-				//create and configure observer/able
-				File temp = new File("D://college 2019//College third year//semester one//Disturbted systems//common//");
-				ServerObservable checkForVillians =  new ServerObservable();
-				ServerObserver retriveVillians = new ServerObserver(socket);
-
-				checkForVillians.addObserver(retriveVillians);
-				checkForVillians.tempFile(temp);
-
-				while (true) {
-					// Listen for a new connection request
-					new ClientHandler(serverSocket.accept()).start();;
-				}
 
 
-			
-
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 
 
->>>>>>> Stashed changes
+		//create and configure observer/able
+		File temp = new File("D://college 2019//College third year//semester one//Disturbted systems//common//");
+		ServerObservable checkForVillians =  new ServerObservable();
+		ServerObserver retriveVillians = new ServerObserver();
+		retriveVillians.setTemp(temp);
 
-		catch (Exception ex) {
-			ex.printStackTrace();
+
+		checkForVillians.addObserver(retriveVillians);
+		checkForVillians.tempFile(temp);
+
+		//loop to keep checking for clients wanting to join
+		while(true) {
+			Socket socket = null;
+
+			try {
+				socket = serverSocket.accept();
+				System.out.println("New connection made");
+				
+				DataOutputStream sendvillain= new DataOutputStream(socket.getOutputStream()); 
+
+
+
+
+
+
+
+			} catch(Exception e) {
+				socket.close();
+				serverSocket.close();
+				e.printStackTrace();
+				
+			}
 		}
 	}
 }
